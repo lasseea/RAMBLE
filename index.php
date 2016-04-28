6366db49754333d2ab2502dd8eea435a526cf153
lasseea <leadk90@gmail.com>
2016-04-28 20:08:53 +0200

    deleting unnecessary files

--- index.php
+++
@@ -1,8 +0,0 @@
-<?php
-include_once 'layout/header.php';
-?>
-
-
-<?php
-include_once 'layout/footer.php'
-?>
--- testfile.php
+++
@@ -1,5 +0,0 @@
-<?php
-$m = new MongoClient();
-$db = $m->university;
-echo "connection success";
-?>
